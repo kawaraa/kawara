7e5fe979fa431f88624073feab8ef1b4577ee615
A Kawara <kawaradev@gmail.com>
2021-02-28 12:44:36 +0100

    Fixed country based item on checkout

--- api/src/infrastructure/repository/checkout-repository.js
+++ api/src/infrastructure/repository/checkout-repository.js
@@ -3,17 +3,16 @@ const { Formatter, CustomError } = require("k-utilities");
 class CheckoutRepository {
   constructor(mySqlProvider) {
     this.mySqlProvider = mySqlProvider;
-    this.error = "Sorry there are items can not be shipped to xx";
+    // this.error = "Sorry there are items can not be shipped to xx";
   }
 
   async checkItem(item, country) {
     const { productNumber, name, type, size } = item;
-    const query = `SELECT t1.pictures, t2.price, t3.cost AS shippingCost, t3.country FROM store.product t1 JOIN store.type t2 ON t1.number = t2.productNumber JOIN store.shipping t3 ON t1.number = t3.productNumber WHERE t1.number = ? AND t1.name = ? AND t2.type = ? AND t2.size = ? GROUP BY t3.country`;
+    const query = `SELECT t1.pictures, t2.price, t3.cost AS shippingCost, t3.country FROM store.product t1 JOIN store.type t2 ON t1.number = t2.productNumber JOIN store.shipping t3 ON t1.number = t3.productNumber WHERE t1.number = ? AND t1.name = ? AND t2.type = ? AND t2.size = ? AND t3.country = ? GROUP BY t3.country`;
 
     const products = await this.mySqlProvider.query(query, [productNumber, name, type, size, country]);
     if (!products[0]) throw new CustomError("Invalid input item");
-
-    if (products[0].country != country) throw new CustomError(this.error.replace("xx", country));
+    // if (products[0].country != country) throw new CustomError(this.error.replace("xx", country));
 
     item.picture = Formatter.stringToArray(products[0].pictures)[0];
     item.price = products[0].price;
