7bdaf2263141c1cc21930e779be7f628115bbcc9
A Kawara <kawaradev@gmail.com>
2021-03-10 11:57:45 +0100

    Added service worker

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -1,5 +1,5 @@
 const filesMustCache = /(googleapis|gstatic)|\.(JS|CSS|SVG|PNG|JPG|jPEG|WEBP|BMP|GIF|ICO|WEBM|MPG|MP2|MP4|MPEG|MPE|MPV|OGG|M4P|M4V|AVI|WMV|MOV|QT|FLV|SWF|AVCHD|JSON)$/gim;
-const staticFileCacheName = "static-files-v1";
+const staticFileCacheName = "static-files-v5";
 const staticFileCachePaths = [
   "/",
   "/offline.html",
@@ -23,70 +23,47 @@ self.addEventListener("activate", async (evt) => {
 
 self.addEventListener("fetch", (evt) => {
   if (evt.request.url.indexOf("http") < 0) evt.respondWith(fetch(evt.request));
-  else if (filesMustCache.test(evt.request.url)) {
-    evt.respondWith(
-      caches
-        .match(evt.request)
-        .then((cachedResponse) => {
-          if (cachedResponse) return cachedResponse;
-          return fetch(evt.request).then((response) => {
-            return caches.open(staticFileCacheName).then((cache) => {
-              cache.put(evt.request, response.clone());
-              return response;
-            });
-          });
-        })
-        .catch((error) => caches.match(staticFileCachePaths[1])) // offline fallback page
-    );
-  } else {
-    evt.respondWith(
-      fetch(evt.request)
-        .then((response) => {
-          return caches.open(staticFileCacheName).then((cache) => {
-            cache.delete("/");
-            cache.add("/");
-            cache.put(evt.request, response.clone());
-            return response;
-          });
-        })
-        .catch((error) => {
-          console.log("SW", error);
-          caches.match(evt.request).then((cachedResponse) => {
-            if (cachedResponse) return cachedResponse;
-            return caches.match(staticFileCachePaths[1]);
-          });
-        }) // offline fallback page
-    );
-  }
+  else if (/auth/.test(evt.request.url)) evt.respondWith(handleAuthRequest(evt.request));
+  else if (/api/.test(evt.request.url)) evt.respondWith(fetch(evt.request));
+  else if (filesMustCache.test(evt.request.url)) evt.respondWith(handleCachedFileRequest(evt.request));
+  else evt.respondWith(handleOtherRequest(evt.request));
 });
 
-// self.addEventListener("fetch", (evt) => {
-//   if (/auth/.test(evt.request.url)) {
-//     evt.respondWith(
-//       caches.open(staticFileCacheName).then((cache) => {
-//         cache.delete("/");
-//         return fetch(evt.request).then((response) => {
-//           cache.add("/");
-//           return response;
-//         });
-//       })
-//     );
-//   } else {
-//     evt.respondWith(
-//       caches
-//         .match(evt.request)
-//         .then((cachedResponse) => {
-//           if (cachedResponse) return cachedResponse;
-//           else if (evt.request.url.indexOf("http") < 0) return fetch(evt.request);
-//           return fetch(evt.request).then((response) => {
-//             if (!filesMustCache.test(evt.request.url)) return response;
-//             return caches.open(staticFileCacheName).then((cache) => {
-//               cache.put(evt.request, response.clone());
-//               return response;
-//             });
-//           });
-//         })
-//         .catch((error) => caches.match(staticFileCachePaths[1])) // offline fallback page
-//     );
-//   }
-// });
+async function handleAuthRequest(request) {
+  try {
+    const cache = await caches.open(staticFileCacheName);
+    cache.delete("/");
+    const response = await fetch(request);
+    cache.add("/");
+    return response;
+  } catch (error) {
+    return handleCachedFileRequest(request);
+  }
+}
+async function handleOtherRequest(request) {
+  try {
+    const response = await fetch(request);
+    await caches.open(staticFileCacheName).then((cache) => cache.put(request, response.clone()));
+    return response;
+  } catch (error) {
+    return handleCachedFileRequest(request);
+  }
+}
+async function handleCachedFileRequest(request) {
+  try {
+    const cachedResponse = await caches.match(request);
+    if (cachedResponse) return cachedResponse;
+    const response = await fetch(request);
+    const cache = await caches.open(staticFileCacheName);
+    cache.put(request, response.clone());
+    return response;
+  } catch (error) {
+    return getDefaultResponse(request);
+  }
+}
+function getDefaultResponse(request) {
+  return caches.match(request).then((cachedResponse) => {
+    if (cachedResponse) return cachedResponse;
+    return caches.match(staticFileCachePaths[1]);
+  });
+}
